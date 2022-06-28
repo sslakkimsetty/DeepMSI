d85c58e37eefc46bf9c967d1cf33a45e4b86f30b
DanGuo1223 <guo.dan@husky.neu.edu>
2022-06-27 22:13:04 -0700

    remove unnecessary files

--- build/lib/classification/__init__.py
+++
@@ -1,7 +0,0 @@
-from classification.model_opt import model_opt
-from classification.build_dataloader import build_dataloader
-from classification.build_model import build_model
-from classification.classify import classify
-from classification.viz import viz
-
-__all__ = ['model_opt','build_dataloader', 'build_model', 'classify', 'viz']
--- build/lib/clustering/__init__.py
+++
@@ -1,1 +0,0 @@
-'''This package connects to the METASPACE website and establishes a workflow for searching, filtering, and downloading mass spectrometry imaging metadata.'''
